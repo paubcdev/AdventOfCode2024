90e22f022f78efd1056b9c0ddfa093b741aba755
Pau Blanes <pau.blanes@asml.com>
2024-12-04 11:35:09 +0100

    AoC 2024, ruff linter applied

--- solutions/day03.py
+++ solutions/day03.py
@@ -10,16 +10,16 @@ part2 = 0
 enabled = True
 
 for i in range(len(D)):
-    if D[i:].startswith('do()'):
+    if D[i:].startswith("do()"):
         enabled = True
     if D[i:].startswith("don't()"):
         enabled = False
-    instruction = re.match(r'mul\((\d{1,3}),(\d{1,3})\)', D[i:])
+    instruction = re.match(r"mul\((\d{1,3}),(\d{1,3})\)", D[i:])
     if instruction is not None:
         x, y = int(instruction.group(1)), int(instruction.group(2))
-        part1 += x*y
+        part1 += x * y
         if enabled:
-            part2 += x*y
+            part2 += x * y
 
 print(part1)
 print(part2)
